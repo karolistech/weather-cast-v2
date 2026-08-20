5db7739afd615fa7ea41bce80221b342577748c4
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-20 17:56:04 +0300

    test safari iOS

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,6 @@ if (rootElement === null) {
   throw new Error("Root element was not found");
 }
 
-document.addEventListener('touchstart', () => {}, { passive: true });
-
 const root = createRoot(rootElement);
 
 root.render(
